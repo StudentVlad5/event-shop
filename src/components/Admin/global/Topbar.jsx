945b1a436785d5d7f56afa44b647ab9c97e86f6f
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-01-09 11:10:51 +0200

    add logout and orders

--- src/components/Admin/global/Topbar.jsx
+++ src/components/Admin/global/Topbar.jsx
@@ -1,44 +1,26 @@
 
-import { Box, IconButton, useTheme} from "@mui/material";
-import { useContext } from "react";
-import { ColorModeContext, tokens } from "theme";
-import { InputBase } from '@mui/material';
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import { Box, IconButton} from "@mui/material";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
+import { useDispatch } from 'react-redux';
+import { logOut } from "../../../redux/auth/operations";
+
 
 
 const Topbar = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const colorMode = useContext(ColorModeContext);
-    return <Box display="flex" justifyContent="space-between" p={2}>
-        {/* SEARCH BAR */}
-        <Box 
-            display="flex" 
-            backgroundColor={colors.primary[400]} 
-            borderRadius="3px">
-                <InputBase sx={{ml:2, flex:1}} placeholder="Search"/>
-                <IconButton type="bytton" sx={{p:1}}>
-                    <SearchIcon/>
-                </IconButton>
-        </Box>
-
+    const dispatch = useDispatch();
+    return <Box display="flex" justifyContent="end" p={2}>
+  
         {/* ICONS */}
         <Box display="flex">
-        <IconButton onClick={colorMode.toggleColorMode}>
-            {theme.palette.mode ==="dark" ? <DarkModeOutlinedIcon/> : <LightModeOutlinedIcon/>}      
-        </IconButton>
         <IconButton>
             <NotificationsOutlinedIcon/>
         </IconButton>
         <IconButton>
             <SettingsOutlinedIcon/>
         </IconButton>
-        <IconButton>
+        <IconButton onClick={()=>{ dispatch(logOut());}}>
             <PersonOutlinedIcon/>
         </IconButton>
         </Box>
