ae809715a0cb08784caa2df422abc42911fab1ac
JuliaGolban <julia.j.shcherban@gmail.com>
2024-01-10 12:57:10 +0200

    add home page

--- src/components/Home/TopEvents/TopEvents.styled.js
+++ src/components/Home/TopEvents/TopEvents.styled.js
@@ -1,3 +1,131 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { theme } from 'components/baseStyles/Variables.styled';
-import { Container, Section } from 'components/baseStyles/CommonStyle.styled';
+import { Section } from 'components/baseStyles/CommonStyle.styled';
+
+export const EventsSection = styled(Section)`
+  padding-bottom: 75px;
+
+  @media screen and (min-width: ${theme.breakpoints.desktop}) {
+    padding-bottom: 120px;
+  }
+`;
+
+export const BtnLinkText = styled(NavLink)`
+  display: block;
+  margin-bottom: 20px;
+  padding-bottom: 2px;
+  text-align: end;
+
+  font-family: ${theme.fonts[0]};
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%; /* 24px */
+  color: ${theme.colors.grey2};
+
+  /* text-decoration: none; */
+  transition: ${theme.transition};
+
+  &:hover,
+  &:focus {
+    color: ${theme.colors.accent};
+  }
+`;
+
+export const EventListItem = styled.li`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ItemImg = styled.img`
+  width: 350px;
+  height: 300px;
+  border-radius: 40px;
+
+  @media screen and (min-width: ${theme.breakpoints.desktop}) {
+    width: 402px;
+    height: 366px;
+  }
+`;
+
+export const DetailsWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  width: 280px;
+  height: 280px;
+  padding: 40px 20px;
+
+  border-radius: 40px;
+  background: ${theme.colors.fon};
+`;
+
+export const Name = styled.span`
+  font-family: ${theme.fonts[1]};
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 120%; /* 24px */
+  color: ${theme.colors.grey1};
+
+  @media screen and (min-width: ${theme.breakpoints.desktop}) {
+    font-size: 24px;
+  }
+`;
+
+export const DateTimeWrapper = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+
+  & li {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
+  }
+`;
+
+export const Head = styled.span`
+  font-family: ${theme.fonts[0]};
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 12.02px; /* 80.133% */
+  text-transform: uppercase;
+  text-align: center;
+  color: ${theme.colors.grey2};
+`;
+
+export const Date = styled.span`
+  font-family: ${theme.fonts[0]};
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 34px; /* 212.5% */
+  text-transform: uppercase;
+  text-align: center;
+  color: ${theme.colors.grey1};
+`;
+
+export const Describe = styled.p`
+  font-family: ${theme.fonts[0]};
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 24px */
+  color: ${theme.colors.grey2};
+  text-align: center;
+`;
--- src/components/Team/Team.styled.js
+++ src/components/Team/Team.styled.js
@@ -1,4 +1,3 @@
-import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { theme } from 'components/baseStyles/Variables.styled';
 
@@ -7,18 +6,16 @@ export const TeamList = styled.ul`
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-  gap: 20px;
+  gap: 30px;
 
   margin-top: 30px;
   margin-bottom: 50px;
   padding: 0 15px;
 
   @media screen and (min-width: ${theme.breakpoints.tablet}) {
-    gap: 25px;
   }
 
   @media screen and (min-width: ${theme.breakpoints.desktop}) {
-    gap: 30px;
     padding: 0;
     margin-top: 70px;
     margin-bottom: 100px;
@@ -35,10 +32,17 @@ export const TeamListItem = styled.li`
   }
 `;
 
-export const ItemImg = styled.img`
+export const ImgBox = styled.div`
   width: 221px;
   height: 221px;
   border-radius: 110.5px;
+  overflow: hidden;
+`;
+
+export const ItemImg = styled.img`
+  width: 221px;
+  height: 221px;
+
   transition: ${theme.transition};
 
   &:hover,
@@ -87,185 +91,36 @@ export const Describe = styled.p`
   }
 `;
 
-export const BtnLink = styled(NavLink)`
-  position: relative;
-  padding: 2px;
-
-  color: ${theme.colors.grey1};
-  font-family: ${theme.fonts[0]};
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 150%; /* 24px */
-
-  transition: ${theme.transition};
-
-  @media screen and (min-width: ${theme.breakpoints.tablet}) {
-  }
-
-  @media screen and (min-width: ${theme.breakpoints.desktop}) {
-    font-size: 16px;
-  }
-
-  &::before,
-  &::after,
-  & span::after,
-  & span::before {
-    content: '';
-    position: absolute;
-    top: 100%;
-    bottom: 0;
-    left: -16px;
-    width: 1px;
-    background: ${theme.colors.accent};
-    transition: ${theme.transition};
-  }
-
-  &::before {
-    right: -16px;
-    left: -16px;
-    width: auto;
-    background: 0;
-    border-right: 1px solid ${theme.colors.accent};
-    border-left: 1px solid ${theme.colors.accent};
-  }
-
-  &::after {
-    right: 0;
-    left: 0;
-    height: 1px;
-    width: auto;
-  }
-
-  & span {
-    position: relative;
-    display: inline-block;
-
-    &::before,
-    &::after {
-      top: -2px;
-      left: auto;
-      right: auto;
-      width: 0;
-      height: 1px;
-      transition: ${theme.transition};
-    }
-
-    &::before {
-      left: -18px;
-    }
-
-    &::after {
-      right: -18px;
-    }
-  }
-
-  &:hover,
-  &:focus {
-    &::before {
-      top: 0;
-    }
-    &::after {
-      right: -16px;
-      left: -16px;
-    }
-
-    & span::before,
-    & span::after {
-      width: 60%;
-    }
-  }
-`;
-
 export const BtnMore = styled.button`
   position: relative;
   display: block;
-  margin: 0 auto;
-  padding: 2px;
 
-  color: ${props => props.theme.white_text};
+  margin: 0 auto;
+  padding: 9px 40px;
+
   font-family: ${theme.fonts[0]};
   font-size: 16px;
   font-style: normal;
   font-weight: 500;
-  line-height: 32.004px; /* 160.02% */
-  text-transform: uppercase;
+  line-height: 30px; /* 150% */
+  text-transform: capitalize;
+  color: ${theme.colors.grey2};
 
   background-color: transparent;
-  border: none;
+  border-radius: 7px;
+  border: 1px solid ${theme.colors.grey2};
+
   transition: ${theme.transition};
   cursor: pointer;
+
   @media screen and (min-width: ${theme.breakpoints.desktop}) {
     font-size: 20px;
   }
 
-  &::before,
-  &::after,
-  & span::after,
-  & span::before {
-    content: '';
-    position: absolute;
-    top: 100%;
-    bottom: 0;
-    left: -16px;
-    width: 1px;
-    background: ${props => props.theme.white_text};
-    transition: ${theme.transition};
-  }
-
-  &::before {
-    right: -16px;
-    left: -16px;
-    width: auto;
-    background: 0;
-    border-right: 1px solid ${props => props.theme.white_text};
-    border-left: 1px solid ${props => props.theme.white_text};
-  }
-
-  &::after {
-    right: 0;
-    left: 0;
-    height: 1px;
-    width: auto;
-  }
-
-  & span {
-    position: relative;
-    display: inline-block;
-    padding: 2px;
-
-    &::before,
-    &::after {
-      top: 0;
-      left: auto;
-      right: auto;
-      width: 0;
-      height: 1px;
-      transition: ${theme.transition};
-    }
-
-    &::before {
-      left: -18px;
-    }
-
-    &::after {
-      right: -18px;
-    }
-  }
-
   &:hover,
   &:focus {
-    &::before {
-      top: 2px;
-    }
-    &::after {
-      right: -16px;
-      left: -16px;
-    }
-
-    & span::before,
-    & span::after {
-      width: 50%;
-    }
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.accent};
+    border: 1px solid ${theme.colors.accent};
   }
 `;
