5ac7ca1bfd2423e5676c6c1a878e3592b22e1f5f
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-01-11 14:00:29 +0200

    change registration form

--- src/components/Events/RegisterModal/RegisterModal.styled.js
+++ src/components/Events/RegisterModal/RegisterModal.styled.js
@@ -1,102 +1,199 @@
+import { Form, Field } from 'formik';
 import styled from 'styled-components';
 import { theme } from 'components/baseStyles/Variables.styled';
-import { BtnLight } from 'components/baseStyles/Button.styled';
-import {
-  EventImages,
-  EventTitle,
-} from '../EventsList/EventList.styled';
+import { Section } from 'components/baseStyles/CommonStyle.styled';
 
-export const SelectedEvent = styled.div`
-  display: flex;
-  flex-direction: row;
-
-  width: 100%;
-  max-height: 80px;
-
-  border-radius: 40px;
-  background: ${theme.colors.white};
-  overflow: hidden;
+export const MessageSection = styled(Section)`
+  padding-top: 0;
+  padding-bottom: 75px;
 
   @media screen and (min-width: ${theme.breakpoints.desktop}) {
-    max-height: 120px;
+    padding-bottom: 120px;
   }
 `;
 
-export const SDataPlaceWrapper = styled.div`
+export const FormList = styled(Form)`
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  margin-bottom: 0;
+  flex-direction: column;
+  gap: 30px;
+
+  @media screen and (min-width: ${theme.breakpoints.desktop}) {
+    gap: 45px;
+    padding: 0 70px;
+  }
+`;
+
+export const FieldsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: center;
+  gap: 20px;
+  width: 100%;
 
   @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+  }
+
+  @media screen and (min-width: ${theme.breakpoints.desktop}) {
+    gap: 70px;
+  }
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
     width: 100%;
   }
-
-  @media screen and (min-width: ${theme.breakpoints.desktop}) {
-    margin-bottom: 12px;
-  }
 `;
 
-export const SDetailsWrapper = styled.div`
-  padding: 5px 10px 5px 5px;
+export const FormLabel = styled.label`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
   width: 100%;
-
-  @media screen and (min-width: ${theme.breakpoints.tablet}) {
-    padding: 15px 20px;
-  }
-
-  @media screen and (min-width: ${theme.breakpoints.desktop}) {
-    width: calc(100% - 256px);
-  }
+  height: 100%;
 `;
 
-export const SEventDate = styled.span`
-  line-height: normal;
-  color: ${theme.colors.grey2};
+export const FormName = styled.span`
+  margin-bottom: 15px;
+
   font-family: ${theme.fonts[0]};
-  font-size: 14px;
+  font-size: 20px;
   font-style: normal;
   font-weight: 500;
-  line-height: 32.004px; /* 228.6% */
-
-  @media screen and (min-width: ${theme.breakpoints.tablet}) {
-    line-height: 32.004px; /* 228.6% */
-  }
+  line-height: normal;
+  color: ${theme.colors.grey1};
 `;
 
-export const SEventImages = styled.img`
-  width: 103px;
+export const FormInput = styled(Field)`
+  width: 100%;
+  padding: 15px;
+
+  font-family: ${theme.fonts[0]};
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: ${theme.colors.grey2};
+
+  background: ${theme.colors.white};
+  border-color: transparent;
+  border: 1px solid ${theme.colors.grey2};
+  border-radius: 10px;
 
   @media screen and (min-width: ${theme.breakpoints.tablet}) {
-    width: 30%;
   }
 
   @media screen and (min-width: ${theme.breakpoints.desktop}) {
-    width: 256px;
+    max-width: 530px;
+    padding: 25px 30px;
+  }
+
+  &:focus-visible {
+    border: 0.5px solid ${theme.colors.accent};
+    outline: none;
+  }
+
+  &::placeholder {
+    color: ${theme.colors.grey2};
+    font-family: ${theme.fonts[0]};
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
   }
 `;
 
-export const SEventTitle = styled.p`
-color: ${theme.colors.black};
-font-family: ${theme.fonts[0]};
-font-size: 15px;
-font-style: normal;
-font-weight: 700;
-line-height: 22.004px;
+export const Error = styled.span`
+  position: absolute;
+  bottom: -10px;
+  right: 0;
+  z-index: 2;
 
-@media screen and (min-width: ${theme.breakpoints.desktop}) {
-  font-size: 18px;
-  line-height: 32.004px; /* 177.8% */
-  margin-bottom: 0;
-}
-`;
+  font-family: ${theme.fonts[0]};
+  font-style: normal;
+  font-weight: 400;
+  font-size: 8px;
+  text-align: right;
+  color: ${theme.colors.red};
 
-export const SBtnLight = styled(BtnLight)`
   @media screen and (min-width: ${theme.breakpoints.tablet}) {
-    padding: 18px 33px;
+    font-size: 12px;
   }
+`;
+
+export const FormBtn = styled.button`
+  position: absolute;
+  bottom: 1px;
+  right: 5px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+
+  border: none;
+  border-radius: 50%;
+  background-color: transparent;
+
+  cursor: pointer;
+  transform: ${theme.transition};
+  transition: ${theme.transition};
+
+  &:hover,
+  &:focus {
+    background-color: ${theme.colors.fon};
+  }
+  &:disabled {
+    svg {
+      fill: ${theme.colors.grey1};
+    }
+  }
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+    width: 32px;
+    height: 32px;
+    margin-left: 24px;
+  }
+`;
+
+export const FormInputMessage = styled.textarea`
+  height: 100%;
+  padding: 8px 30px 8px 20px;
+
+  font-family: ${theme.fonts[0]};
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: ${theme.colors.grey2};
+
+  background: ${theme.colors.white};
+  border: 1px solid ${theme.colors.grey2};
+  border-radius: 10px;
+
+  @media screen and (min-width: ${theme.breakpoints.tablet}) {
+  }
+
   @media screen and (min-width: ${theme.breakpoints.desktop}) {
-    width: 190px;
-    margin: 0 auto 0 0;
+    width: 530px;
+    padding: 25px 30px;
+  }
+
+  &:focus-visible {
+    border: 0.5px solid ${theme.colors.accent};
+    outline: none;
+  }
+
+  &::placeholder {
+    color: ${theme.colors.grey2};
+    font-family: ${theme.fonts[0]};
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
   }
 `;
