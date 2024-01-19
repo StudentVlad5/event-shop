e5832a2cb0dc065474e19cdf7920abaafc68b3f3
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-01-19 20:50:50 +0200

    fix form registration

--- src/pages/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage.jsx
@@ -8,51 +8,11 @@ import { EventDetails } from 'components/Events/EventDetails/EventDetails';
 import { StatusContext } from 'components/ContextStatus/ContextStatus';
 
 const EventDetailsPage = () => {
-  const [event, setEvent] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const routeParams = useParams();
   const { selectedLanguage } = useContext(StatusContext);
   const [activeEvents, setActiveEvents] = useState([]);
-
-  // useEffect(() => {
-  //   async function getData() {
-  //     setIsLoading(true);
-  //     try {
-  //       const { data } = await fetchData(`/events/${routeParams.id}`);
-  //       if (!data) {
-  //         return onFetchError('Whoops, something went wrong');
-  //       }
-
-  //       const langData = [
-  //         {
-  //           _id: data._id,
-  //           article_event: data.article_event,
-  //           name: data.name,
-  //           description: data.description,
-  //           duration: data.duration,
-  //           rating: data.rating,
-  //           category: data.category,
-  //           category_second: data.category_second,
-  //           category_third: data.category_third,
-  //           specialistId: data.specialistId,
-  //           image: data.image,
-  //           image_1: data.image_1,
-  //           image_2: data.image_2,
-  //           ...data[selectedLanguage],
-  //         },
-  //       ];
-  //       setEvent(langData[0]);
-  //     } catch (error) {
-  //       setError(error);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   }
-  //   if (routeParams.id !== '' && routeParams !== undefined) {
-  //     getData();
-  //   }
-  // }, [routeParams.id]);
 
   useEffect(() => {
     async function getData() {
