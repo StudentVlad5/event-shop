a216e396d4045588eef67667c591ac1cd4f236f9
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-01-23 07:07:53 +0200

    change BASE_URL

--- src/helpers/constants.jsx
+++ src/helpers/constants.jsx
@@ -1,5 +1,8 @@
-export const BASE_URL = 'https://event-shop-backend.vercel.app/api';
-export const BASE_URL_IMG = 'https://event-shop-backend.vercel.app/uploads/';
+// export const BASE_URL = 'https://event-shop-backend.vercel.app/api';
+// export const BASE_URL_IMG = 'https://event-shop-backend.vercel.app/uploads/';
+
+export const BASE_URL = 'https://event-shop-backend.onrender.com/api';
+export const BASE_URL_IMG = 'https://event-shop-backend.onrender.com/uploads/';
 
 // export const BASE_URL = 'http://localhost:3030/api';
 // export const BASE_URL_IMG = 'http://localhost:3030/uploads/';
