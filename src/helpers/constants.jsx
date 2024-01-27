34d45986b4832f43f342e9a3c524d6dec89e75d6
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-01-27 08:48:08 +0200

    add back

--- src/helpers/constants.jsx
+++ src/helpers/constants.jsx
@@ -1,7 +1,7 @@
-// export const BASE_URL = 'https://event-shop-backend.vercel.app/api';
+export const BASE_URL = 'https://event-shop-backend.vercel.app/api';
 // export const BASE_URL_IMG = 'https://event-shop-backend.vercel.app/uploads/';
 
-export const BASE_URL = 'https://event-shop-backend.onrender.com/api';
+// export const BASE_URL = 'https://event-shop-backend.onrender.com/api';
 // export const BASE_URL_IMG = 'https://event-shop-backend.onrender.com/uploads/';
 
 // export const BASE_URL = 'http://localhost:3030/api';
